6cc5bbd900a7fe5466749d53f8ce1f9ca3d20e0c
Wei-Ting,Chou <spkwm@gmail.com>
2017-01-16 10:11:52 +0800

    Update0.2

---
+++ FINAL BIG ASSIGN/Assets/Board.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class Board : MonoBehaviour {
+
+
+    public Vector2 Difference;
+    public Vector2 Differ;
+    public float PeriodTime;
+	// Use this for initialization
+	void Start () 
+    {
+        Differ = Difference;
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () 
+    {
+        Difference = Differ;
+        if(Time.time % PeriodTime < 0.5*PeriodTime)
+        {
+            translate(Difference);
+        }
+        else
+        {
+            translate(-Difference);
+        }
+	}
+    private void translate(Vector2 Difference)
+    {
+        Difference *= Time.fixedDeltaTime;
+        transform.Translate(Difference);
+    }
+}
---
+++ FINAL BIG ASSIGN/Assets/springboard.cs
@@ -0,0 +1,70 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent (typeof (Rigidbody2D))]
+[RequireComponent (typeof (PolygonCollider2D))]
+public class springboard : MonoBehaviour {
+
+    private PolygonCollider2D PC2D;
+    //自己的碰撞器
+    private Rigidbody2D RD;
+    //自己的剛體
+    private PolygonCollider2D player;
+    //玩家的碰撞器
+    private Transform  initial;
+    //初始位置
+    public ForceMode2D FM;
+    public Vector3 V;
+    public int a,b,c,d,e,f;
+
+	// Use this for initialization
+	void Start () 
+    {
+        PC2D = GetComponent<PolygonCollider2D>();
+        RD = GetComponent<Rigidbody2D>();
+        PC2D.isTrigger = true;
+        player = GameObject.FindGameObjectWithTag("Player").GetComponent<PolygonCollider2D>();
+        initial = gameObject.GetComponent<Transform>();
+        V = new Vector3(0,1,0);
+	
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () 
+    {
+        if (PC2D.IsTouching(player) == true)
+        {
+            downward();
+            ++a;
+        }
+        else
+        {
+            if(gameObject.transform.position.y > initial.position.y)
+            {
+                downward();
+                ++b;
+            }
+            else if(gameObject.transform.position.y < initial.position.y)
+            {
+                upward();
+                ++c;
+            }
+            else
+            {
+                ++d;
+                return;
+            }
+
+        }
+	}
+    private void downward()
+    {
+        RD.AddForce(-V, FM);
+        ++e;
+    }
+    private void upward()
+    {
+        RD.AddForce(V, FM);
+        ++f;
+    }
+}
